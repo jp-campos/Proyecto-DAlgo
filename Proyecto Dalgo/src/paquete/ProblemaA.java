48c2ac1e0e1a65a237f5f93caeeea4200ea5ac6b
jp-campos <jp.campos@uniandes.edu.co>
2018-12-08 13:56:10 -0500

    Main problema A

--- Proyecto Dalgo/src/paquete/ProblemaA.java
+++ Proyecto Dalgo/src/paquete/ProblemaA.java
@@ -1,12 +1,53 @@
 package paquete;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class ProblemaA {
 
+	
+	public int subArregloAsc (int n, int [ ]numeros)
+	{
+	
+		
+		
+		return 0; 
+	}
 	
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ProblemaA instancia = new ProblemaA();
+		try ( 
+			InputStreamReader is= new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(is);
+		) { 
+			String line = br.readLine();
+				
+			while(line!=null && line.length()>0 && !"0".equals(line)) {
+				
+				
+				int n = Integer.parseInt(line); 
+				
+				line = br.readLine();
+				
+				final String [] dataStr = line.split(" ");
+				final int[] numeros = Arrays.stream(dataStr).mapToInt(f->Integer.parseInt(f)).toArray();
+				
+				
+				System.out.println(instancia.subArregloAsc(n, numeros)); 
+				
+			
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 }
--- Proyecto Dalgo/src/paquete/ProblemaB.java
+++ Proyecto Dalgo/src/paquete/ProblemaB.java
@@ -1,5 +1,11 @@
 package paquete;
 
 public class ProblemaB {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
